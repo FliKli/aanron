cb6d7ba94a9d47f551def9b94f394ed6a59866b4
FliKli <torosananton@gmail.com>
2024-04-20 11:18:53 +0300

    aanron

--- click.js
+++ click.js
@@ -1,20 +1,31 @@
 
+
+
 var btn = document.getElementById("grop"); 
  
 btn.onclick = function() { 
     console.log('hi') 
- 
-    const modal = document.getElementById('content') 
-    console.log(modal) 
+    if (contentWrapper.style.display === 'none') {
+        contentWrapper.style.display = 'block'
+        const arrGames = ['test' , 'zdfklnmadbvgjkzdfnjkbv', 'test3', 'hjjknjknnj'] 
+
+const contentWrapper = document.getElementById('content-wraper') 
+
+arrGames.forEach((elem) => { 
+    console.log(elem) 
+    const elemNew = document.createElement('div') 
+    elemNew.classList.add('frog') 
+    elemNew.textContent = elem 
+    contentWrapper.appendChild(elemNew) 
+})
+
+    } else {
+        
+        contentWrapper.style.display = 'none'
+    }
+    
+
      
-    const arrGames = ['test' , 'zdfklnmadbvgjkzdfnjkbv', 'test3', 'hjjknjknnj'] 
-    //.filter((elem) => elem.includes('test')) 
-    arrGames.forEach((elem) => { 
-        console.log(elem) 
-        const elemNew = document.createElement('div') 
-        elemNew.classList.add('frog') 
-        elemNew.textContent = elem 
-        modal.appendChild(elemNew) 
-    })
+  
 } 
  
